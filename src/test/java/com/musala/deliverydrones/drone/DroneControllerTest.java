7646d18e9ba99e025393a066bf98229a455dbbf5
Prajwol Prajapati <prajwolprajapati123@gmail.com>
2023-03-21 09:29:31 +0545

    Add basic JUnit test for load medication in drones.

--- src/test/java/com/musala/deliverydrones/drone/DroneControllerTest.java
+++ src/test/java/com/musala/deliverydrones/drone/DroneControllerTest.java
@@ -1,5 +1,7 @@
 package com.musala.deliverydrones.drone;
 
+import com.musala.deliverydrones.medication.Medication;
+import com.musala.deliverydrones.medication.MedicationDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,4 +32,45 @@ class DroneControllerTest {
     }
 
 
+    @Test
+    void loadMedication() {
+        DroneRequestDto droneRequest = new DroneRequestDto();
+        droneRequest.setBattery(50);
+        droneRequest.setModel(Model.CRUISER_WEIGHT.toString());
+        droneRequest.setSerialNumber("SN-MOCK");
+        droneRequest.setState(State.IDLE.toString());
+        droneRequest.setWeightLimit(200.00);
+
+        DroneResponseDto mockDrone = droneController.registerDrone(droneRequest);
+
+        MedicationDto medicationDto = new MedicationDto();
+        medicationDto.setName("Med-1");
+        medicationDto.setCode("CODE_1");
+        medicationDto.setWeight(20.00);
+        medicationDto.setImage("data:image/jpg;base64,SUQsasasasas909090==");
+
+        Drone drone = droneController.loadMedication(mockDrone.getId(), medicationDto);
+        Medication medication = drone.getMedications().stream().findFirst().get();
+
+        Assertions.assertEquals(medication.getName(), medicationDto.getName());
+        Assertions.assertEquals(medication.getCode(), medicationDto.getCode());
+        Assertions.assertEquals(medication.getWeight(), medicationDto.getWeight());
+        Assertions.assertEquals(medication.getImage(), medicationDto.getImage());
+    }
+
+    @Test
+    void getAvailableDrones() {
+    }
+
+    @Test
+    void getDroneMedication() {
+    }
+
+    @Test
+    void getDrone() {
+    }
+
+    @Test
+    void updateBatteryInfo() {
+    }
 }
